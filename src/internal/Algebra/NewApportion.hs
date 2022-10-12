fe922706ebd992d70bd64cb980ac12ae9ccae924
Jonathan Knowles <mail@jonathanknowles.net>
2022-10-12 10:22:37 +0000

    Split `Apportion` into `Apportion` and `BalancedApportion`.

--- src/internal/Algebra/NewApportion.hs
+++ src/internal/Algebra/NewApportion.hs
@@ -25,93 +25,107 @@ import qualified Data.List.NonEmpty as NE
 import qualified Data.Strict.Map as Map
 
 --------------------------------------------------------------------------------
--- Class
+-- Apportion
 --------------------------------------------------------------------------------
 
 class (Eq a, Semigroup a) => Apportion a where
 
-    type Exact a
     type Weight a
 
     apportion :: a -> NonEmpty (Weight a) -> (a, NonEmpty a)
-    apportionDeviation :: a -> Exact a -> Ratio Natural
-    apportionExact :: a -> NonEmpty (Weight a) -> (Exact a, NonEmpty (Exact a))
-    apportionMaybe :: a -> NonEmpty (Weight a) -> Maybe (NonEmpty a)
-
-    -- how to do apportionOrdering ?
-    -- perhaps it needs MonoidMap with values that are LeftReductive.
-
     default apportion
         :: Monoid a => a -> NonEmpty (Weight a) -> (a, NonEmpty a)
     apportion a as = case apportionMaybe a as of
         Nothing -> (a, mempty <$ as)
         Just bs -> (mempty, bs)
 
+    apportionMaybe :: a -> NonEmpty (Weight a) -> Maybe (NonEmpty a)
     default apportionMaybe
         :: (Eq a, Monoid a) => a -> NonEmpty (Weight a) -> Maybe (NonEmpty a)
     apportionMaybe a as = case apportion a as of
        (b, bs) | b == mempty -> Just bs
        (_, _) -> Nothing
 
-apportionLawBalance :: Apportion a => a -> NonEmpty (Weight a) -> Bool
-apportionLawBalance a ws =
-    all (<= 1) $ NE.zipWith apportionDeviation
-        (uncurry NE.cons (apportion      a ws))
-        (uncurry NE.cons (apportionExact a ws))
-
 apportionLawLength :: Apportion a => a -> NonEmpty (Weight a) -> Bool
 apportionLawLength a ws =
     apportion a ws & \(_, rs) -> length rs == length ws
-
-apportionLawLengthExact :: Apportion a => a -> NonEmpty (Weight a) -> Bool
-apportionLawLengthExact a ws =
-    apportionExact a ws & \(_, rs) -> length rs == length ws
 
 apportionLawSum :: Apportion a => a -> NonEmpty (Weight a) -> Bool
 apportionLawSum a ws =
     apportion a ws & \(r, rs) -> sconcat (NE.cons r rs) == a
 
 --------------------------------------------------------------------------------
--- Instance: Sum Natural
+-- BalancedApportion
+--------------------------------------------------------------------------------
+
+class Apportion a => BalancedApportion a where
+
+    type Exact a
+
+    apportionDeviation :: a -> Exact a -> Ratio Natural
+    apportionExact :: a -> NonEmpty (Weight a) -> (Exact a, NonEmpty (Exact a))
+
+    -- how to do apportionOrdering ?
+    -- perhaps it needs MonoidMap with values that are LeftReductive.
+
+apportionLawBalance :: BalancedApportion a => a -> NonEmpty (Weight a) -> Bool
+apportionLawBalance a ws =
+    all (<= 1) $ NE.zipWith apportionDeviation
+        (uncurry NE.cons (apportion      a ws))
+        (uncurry NE.cons (apportionExact a ws))
+
+
+apportionLawLengthExact
+    :: BalancedApportion a => a -> NonEmpty (Weight a) -> Bool
+apportionLawLengthExact a ws =
+    apportionExact a ws & \(_, rs) -> length rs == length ws
+
+--------------------------------------------------------------------------------
+-- Instances: Sum Natural
 --------------------------------------------------------------------------------
 
 instance Apportion (Sum Natural) where
-    type Exact (Sum Natural) = Ratio Natural
     type Weight (Sum Natural) = Natural
     apportion = undefined
-    apportionExact = undefined
     apportionMaybe = undefined
+
+instance BalancedApportion (Sum Natural) where
+    type Exact (Sum Natural) = Ratio Natural
+    apportionExact = undefined
     apportionDeviation = undefined
 
 --------------------------------------------------------------------------------
--- Instance: List
+-- Instances: List
 --------------------------------------------------------------------------------
 
 instance Eq a => Apportion [a] where
-    type Exact [a] = Ratio Natural
     type Weight [a] = Natural
     apportion = undefined
-    apportionExact = undefined
     apportionMaybe = undefined
 
+instance Eq a => BalancedApportion [a] where
+    type Exact [a] = Ratio Natural
+    apportionExact = undefined
     apportionDeviation :: [a] -> Ratio Natural -> Ratio Natural
     apportionDeviation as = distanceRatioNatural
         (naturalToRatio $ naturalLength as)
 
 --------------------------------------------------------------------------------
--- Instance: Map
+-- Instances: Map
 --------------------------------------------------------------------------------
 
 instance (Ord k, Eq v) => Apportion (Map k v) where
-    type Exact (Map k v) = Exact v
     type Weight (Map k v) = Map k v
     apportion = undefined
-    apportionExact = undefined
     apportionMaybe = undefined
+
+instance (Ord k, Eq v) => BalancedApportion (Map k v) where
+    type Exact (Map k v) = Exact v
+    apportionExact = undefined
     apportionDeviation = undefined
 
 --------------------------------------------------------------------------------
--- Instance: Map Keys
+-- Instances: Map Keys
 --------------------------------------------------------------------------------
 
 newtype Keys a = Keys
@@ -119,52 +133,59 @@ newtype Keys a = Keys
     deriving (Eq, Monoid, Semigroup, Show)
 
 instance (Ord k, Eq v) => Apportion (Keys (Map k v)) where
-    type Exact (Keys (Map k v)) = Ratio Natural
     type Weight (Keys (Map k v)) = Natural
     apportion = undefined
-    apportionExact = undefined
     apportionMaybe = undefined
 
+instance (Ord k, Eq v) => BalancedApportion (Keys (Map k v)) where
+    type Exact (Keys (Map k v)) = Ratio Natural
+    apportionExact = undefined
     apportionDeviation :: Keys (Map k v) -> Ratio Natural -> Ratio Natural
     apportionDeviation (Keys m) = distanceRatioNatural
         (naturalToRatio $ naturalLength $ Map.keys m)
 
 --------------------------------------------------------------------------------
--- Instance: Map Values
+-- Instances: Map Values
 --------------------------------------------------------------------------------
 
 newtype Values a = Values
     { unValues :: a }
     deriving (Eq, Monoid, Semigroup, Show)
 
-instance (Ord k, Eq v, Apportion v) => Apportion (Values (Map k v)) where
-    type Exact (Values (Map k v)) = Exact v
+instance (Ord k, Eq v, Apportion v) =>
+    Apportion (Values (Map k v))
+  where
     type Weight (Values (Map k v)) = Weight v
     apportion = undefined
-    apportionExact = undefined
     apportionMaybe = undefined
+
+instance (Ord k, Eq v, BalancedApportion v) =>
+    BalancedApportion (Values (Map k v))
+  where
+    type Exact (Values (Map k v)) = Exact v
+    apportionExact = undefined
     apportionDeviation = undefined
 
 --------------------------------------------------------------------------------
--- Apportioning with equal weights
+-- BalancedApportioning with equal weights
 --------------------------------------------------------------------------------
 
 apportionEqual
-    :: (Apportion a, Integral (Weight a))
+    :: (BalancedApportion a, Integral (Weight a))
     => a
     -> NonEmpty void
     -> NonEmpty a
 apportionEqual a ws = snd (apportion a (1 <$ ws))
 
 apportionEqualN
-    :: (Apportion a, Integral (Weight a))
+    :: (BalancedApportion a, Integral (Weight a))
     => a
     -> Int
     -> NonEmpty a
 apportionEqualN a n =
     apportionEqual a (() :| replicate (max 0 (n - 1)) ())
 
-bipartition :: (Apportion a, Integral (Weight a)) => a -> (a, a)
+bipartition :: (BalancedApportion a, Integral (Weight a)) => a -> (a, a)
 bipartition = (NE.head &&& NE.last) . flip apportionEqual (() :| [()])
 
 --------------------------------------------------------------------------------
@@ -185,7 +206,7 @@ apportionEqualMapKeys
 apportionEqualMapKeys m ws = unKeys <$> apportionEqual (Keys m) ws
 
 apportionEqualMapValues
-    :: (Ord k, Eq v, Apportion v, Integral (Weight v))
+    :: (Ord k, Eq v, BalancedApportion v, Integral (Weight v))
     => Map k v -> NonEmpty void -> NonEmpty (Map k v)
 apportionEqualMapValues m ws = unValues <$> apportionEqual (Values m) ws
 
