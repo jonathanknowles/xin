d881cd059cd790de93ff77474e79163596862237
Jonathan Knowles <mail@jonathanknowles.net>
2022-11-03 12:00:29 +0000

    Add default methods for SizeDivisible.

--- src/internal/Data/SizeDivisible.hs
+++ src/internal/Data/SizeDivisible.hs
@@ -1,3 +1,7 @@
+{- HLINT ignore "Use drop" -}
+{- HLINT ignore "Use take" -}
+{- HLINT ignore "Use splitAt" -}
+
 module Data.SizeDivisible
     ( SizeDivisible (..)
     , splitAtMany
@@ -19,7 +23,7 @@ import Numeric.Natural
     ( Natural )
 
 import Prelude hiding
-    ( splitAt )
+    ( drop, splitAt, take )
 
 import qualified Data.List as L
 import qualified Data.List.NonEmpty as NE
@@ -27,34 +31,48 @@ import qualified Data.Strict.Map as Map
 import qualified Data.Strict.Set as Set
 
 class Sized a => SizeDivisible a where
+
     drop :: Size a -> a -> a
+    default drop :: Size a -> a -> a
+    drop s a = snd (splitAt s a)
+
     take :: Size a -> a -> a
+    default take :: Size a -> a -> a
+    take s a = fst (splitAt s a)
+
     splitAt :: Size a -> a -> (a, a)
+    default splitAt :: Size a -> a -> (a, a)
+    splitAt s a = (take s a, drop s a)
+
+    {-# MINIMAL (drop, take) | splitAt #-}
 
 instance SizeDivisible [a] where
-    drop = L.drop . naturalToInt
-    take = L.take . naturalToInt
     splitAt = L.splitAt . naturalToInt
 
 instance SizeDivisible (Map k v) where
-    drop = Map.drop . naturalToInt
-    take = Map.take . naturalToInt
     splitAt = Map.splitAt . naturalToInt
 
 instance SizeDivisible (Set a) where
-    drop = Set.drop . naturalToInt
-    take = Set.take . naturalToInt
     splitAt = Set.splitAt . naturalToInt
 
-splitAtMany :: forall a. SizeDivisible a => NonEmpty (Size a) -> a -> NonEmpty a
+splitAtMany
+    :: forall a. SizeDivisible a
+    => NonEmpty (Size a)
+    -> a
+    -> NonEmpty a
 splitAtMany chunkSizes a = NE.unfoldr makeChunk (chunkSizes, a)
   where
-    makeChunk :: (NonEmpty (Size a), a) -> (a, Maybe (NonEmpty (Size a), a))
+    makeChunk
+        :: (NonEmpty (Size a), a) -> (a, Maybe (NonEmpty (Size a), a))
     makeChunk (l :| mls, r) = case NE.nonEmpty mls of
         Just ls -> (prefix, Just (ls, suffix))
         Nothing -> (r, Nothing)
       where
         (prefix, suffix) = splitAt l r
+
+--------------------------------------------------------------------------------
+-- Internal functions
+--------------------------------------------------------------------------------
 
 naturalToInt :: Natural -> Int
 naturalToInt = fromMaybe maxBound . intCastMaybe @Natural @Int
