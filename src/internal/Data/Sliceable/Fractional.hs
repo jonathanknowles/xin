ed62928aaccd4b4170171652ce616ef5346281f5
Jonathan Knowles <mail@jonathanknowles.net>
2022-11-07 01:06:11 +0000

    Add `FractionalSlice`.

---
+++ src/internal/Data/Sliceable/Fractional.hs
@@ -0,0 +1,80 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Data.Sliceable.Fractional where
+
+import Algebra.ExactBounded
+    ( ExactBounded (..) )
+import Algebra.PartialOrd.Extended
+    ( Infix (..), PartialOrd (..) )
+import Data.Ratio
+    ( Ratio )
+import Data.Monoid
+    ( Sum (..) )
+import Data.Monoid.Monus.Extended
+    ( (<\>) )
+import Data.Sliceable
+    ( Sliceable )
+import Numeric.Natural
+    ( Natural )
+
+import Prelude hiding
+    ( drop, splitAt, take )
+
+import qualified Data.Sliceable as Sliceable
+
+newtype FractionalSlice f a = FractionalSlice (f (a, Ratio Natural))
+
+deriving instance Eq (f (a, Ratio Natural)) => Eq (FractionalSlice f a)
+
+instance Eq (f (a, Ratio Natural)) => PartialOrd (Infix (FractionalSlice f a))
+  where
+    leq = undefined
+
+instance
+    ( Eq (f (a, Ratio Natural))
+    , PartialOrd (Infix (f a))
+    ) =>
+    ExactBounded (Infix (FractionalSlice f a)) (Infix (f a))
+  where
+    exact = undefined
+    lowerBound = undefined
+    upperBound = undefined
+
+instance Foldable f => Sliceable (FractionalSlice f a) where
+    type SliceableSize (FractionalSlice f a) = Ratio Natural
+    drop = drop
+    take = take
+    splitAt = splitAt
+    size = size
+
+size :: Foldable f => FractionalSlice f a -> Ratio Natural
+size (FractionalSlice as) = getSum $ foldMap (Sum . snd) as
+
+isPrefixOf
+    :: Foldable f
+    => Eq (f (a, Ratio Natural))
+    => FractionalSlice f a
+    -> FractionalSlice f a
+    -> Bool
+isPrefixOf a b = a == take (size a) b
+
+isSuffixOf
+    :: Foldable f
+    => Eq (f (a, Ratio Natural))
+    => FractionalSlice f a
+    -> FractionalSlice f a
+    -> Bool
+isSuffixOf a b = a == drop (getSum (Sum (size b) <\> Sum (size a))) b
+
+drop :: Ratio Natural -> FractionalSlice f a -> FractionalSlice f a
+drop = undefined
+
+take :: Ratio Natural -> FractionalSlice f a -> FractionalSlice f a
+take = undefined
+
+splitAt
+    :: Ratio Natural
+    -> FractionalSlice f a
+    -> (FractionalSlice f a, FractionalSlice f a)
+splitAt r f = (take r f, drop r f)
