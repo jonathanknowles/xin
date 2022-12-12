jonathanknowles/csng

Revise Roundable

src/internal/Value.hs
@@ -264,12 +264,12 @@ newtype FractionalCoin a = FractionalCoin (MonoidMap a FractionalCoinValue)
         )
 
 instance Roundable FractionalCoinValue CoinValue where
-    roundU = unpacked ceiling
-    roundD = unpacked floor
+    roundUp = unpacked ceiling
+    roundDown = unpacked floor
 
 instance Ord a => Roundable (FractionalCoin a) (Coin a) where
-    roundU = unpacked $ MonoidMap.map roundU
-    roundD = unpacked $ MonoidMap.map roundD
+    roundUp = unpacked $ MonoidMap.map roundUp
+    roundDown = unpacked $ MonoidMap.map roundDown
 
 --------------------------------------------------------------------------------
 -- Conversions

jonathanknowles/xin

src/internal/Value.hs
@@ -264,12 +264,12 @@ newtype FractionalCoin a = FractionalCoin (MonoidMap a FractionalCoinValue)
         )
 
 instance Roundable FractionalCoinValue CoinValue where
-    roundU = unpacked ceiling
-    roundD = unpacked floor
+    roundUp = unpacked ceiling
+    roundDown = unpacked floor
 
 instance Ord a => Roundable (FractionalCoin a) (Coin a) where
-    roundU = unpacked $ MonoidMap.mapValues roundU
-    roundD = unpacked $ MonoidMap.mapValues roundD
+    roundUp = unpacked $ MonoidMap.mapValues roundUp
+    roundDown = unpacked $ MonoidMap.mapValues roundDown
 
 --------------------------------------------------------------------------------
 -- Conversions